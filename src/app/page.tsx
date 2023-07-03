0fd8a791f0e0d77af899f9a10fb7c753a1aafb13
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-07-03 18:09:41 -0500

    Hook Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Banner from '#/Banner.png'
 import Navbar from '@/components/Navbar'
+import TestHook from '@/hooks/testHook'
 
 export default function Home() {
   return (
@@ -10,8 +11,10 @@ export default function Home() {
       
       <Image src={Banner} alt="Digital Alchemyst Studios" width={1200} height={350} />
       <h4 className="text-lg font-bold text-center">
-          Welcome to Next.js 13.4 + TypeScript 4.5 + Tailwind CSS 3.0 + Jest 27.2 + React Testing Library 12.1 + Cypress 9.5 + Storybook 6.3 + ESLint 8.1 + Prettier 2.5 + Husky 7.0 + Lint-Staged 11.2 + Commitlint 13.1 + Github Actions 5.1 + Node 16.13 Biolerplate by Digitl Alchemyst @ Digital Alchemyst Studios
+          Welcome to Next.js 13.4 Biolerplate + TypeScript 4.5 + Tailwind CSS 3.0 + Jest 27.2 + React Testing Library 12.1 + Cypress 9.5 + Storybook 6.3 + ESLint 8.1 + Prettier 2.5 + Husky 7.0 + Lint-Staged 11.2 + Commitlint 13.1 + Github Actions 5.1 + Node 16.13 Biolerplate by Digitl Alchemyst @ Digital Alchemyst Studios
       </h4>
+      <button className='bg-zing-400 cursor-pointer border border-zinc-700 p-3 rounded-lg my-2'> Visit the Github </button>
+      <TestHook />
       
     </main>
     </div>
--- src/hooks/testHook.tsx
+++ src/hooks/testHook.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from "react";
 // write a use staate hook that will set a boolean value for testBool to false
 
@@ -5,10 +7,30 @@ const TestHook = () => {
     const [testBool, setTestBool] = useState(false);
     return (
         <div>
-        <button onClick={() => setTestBool(!testBool)}>Click Me</button>
-        <p>{testBool ? "true" : "false"}</p>
+        <button className='bg-zinc-300 cursor-pointer border border-zinc-600 p-3 rounded-lg my-2' onClick={() => setTestBool(!testBool)}>Change State</button>
+        <p>Boolean State: {testBool ? "true" : "false"}</p>
         </div>
     );
     }
 export default TestHook;
 
+
+
+
+// test
+// /**
+//  * @jest-environment jsdom
+//  */
+
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import '@testing-library/jest-dom'
+// import testHook from '../../src/hooks/testHook';
+
+// describe('Test Hook', () => {
+//     it('Is the hook setting the Boolean?', () => {
+//         render({testHook});
+//         expect(screen.getByText('false')).toBeInTheDocument();
+//     });
+//     }
+// );
